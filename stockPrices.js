ae72674ce4dae0c8c42e1136fdb9d91d8c8193c4
Garrett Pace <elderpace6@gmail.com>
2023-04-25 19:48:48 -0600

    done!

--- stockPrices.js
+++ stockPrices.js
@@ -3,12 +3,13 @@ let prices = [2, 3, 10, 6, 4, 8, 1]
 function best(prices) {
     let minPrice = prices[0]
     let maxProfit = 0
-    for (let i = 1; i < prices.length; i++) {
+    for (let i = 0; i < prices.length; i++) {
     if (prices[i] < minPrice) {
         minPrice = prices[i]
     } else if (prices[i] - minPrice > maxProfit) {
         maxProfit = prices[i] - minPrice
     }
+    console.log(`i = ${i}, i value = ${prices[i]}, minPrice = ${minPrice}, maxProfit = ${maxProfit}`)
 }
 return maxProfit
 }
